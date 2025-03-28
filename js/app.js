ca307c3098fd456a8af860c6ec447f3acb98e7b9
lenonbr <lenon.br@hotmail.com>
2025-03-28 10:12:01 -0300

    função principal e demais variáveis criadas

---
+++ js/app.js
@@ -0,0 +1,30 @@
+function comprar() {
+    let tipo = parseInt(document.getElementById('tipo-ingresso').value);
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+    if(tipo == 'pista') {
+        comprarPista(qtd);
+    }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = document.getElementById('qtd-pista');
+
+    if(qtd > qtdPista) {
+        alert('Não há mais ingressos disponíveis para Pista.');
+    }
+    else {
+        
+        alert('Compra realizada com sucesso.')
+    }
+
+    alert(qtdPista)
+}
+
+function comprarInferior() {
+
+}
+
+function comprarSuperior() {
+    
+}
