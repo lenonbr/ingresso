2e8e7caf34e2c9dbd69f524b6f3220b7207848fa
lenonbr <lenon.br@hotmail.com>
2025-03-28 11:15:11 -0300

    adicionada função comprarPista

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
+comprar();
 function comprar() {
-    let tipo = parseInt(document.getElementById('tipo-ingresso').value);
+    let tipo = document.getElementById('tipo-ingresso');
     let qtd = parseInt(document.getElementById('qtd').value);
 
-    if(tipo == 'pista') {
+    if(tipo.value == 'pista') {
         comprarPista(qtd);
     }
+    if(tipo.value == 'inferior') {
+        comprarInferior(qtd);
+    }
+    if(tipo.value == 'superior') {
+        comprarSuperior(qtd);
+    };
 }
 
 function comprarPista(qtd) {
-    let qtdPista = document.getElementById('qtd-pista');
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
 
     if(qtd > qtdPista) {
         alert('Não há mais ingressos disponíveis para Pista.');
     }
     else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista
         alert('Compra realizada com sucesso.')
     }
-
-    alert(qtdPista)
 }
 
-function comprarInferior() {
-
-}
-
-function comprarSuperior() {
-    
-}
